376fed0739c955e4ad4520fb8136f434f2423cef
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-05-24 16:56:12 +0300

    modified my index.html,index.css to register.html and register.css and create a new file for the index.html and index.css

---
+++ assets/js/register.js
@@ -0,0 +1,11 @@
+onclick.addEventlistener
+
+function register(){
+    
+    var firstname =document.getElementById("firstname");
+    if(firstname=="null"||firstname==""){
+        alert("firstname cannot be empty");
+    }
+
+
+}
